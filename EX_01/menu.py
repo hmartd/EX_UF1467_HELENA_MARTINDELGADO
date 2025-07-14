c20d595d49f35b02ea53873a8d6fd16145865994
Helena Martin <heleenamartiin@gmail.com>
2025-07-14 13:29:38 +0200

    EX_01 editado

--- EX_01/menu.py
+++ EX_01/menu.py
@@ -5,34 +5,38 @@ menu = {'Pizza Margarita': 8.99,
         'Agua Mineral': 1.5}
 
 def place_order(selected_food, money):
-    
-    if selected_food not in menu:
-        raise ValueError("El alimento seleccionado no está en el menú")
-    if menu[selected_food] > money:
-        raise ValueError("No se disponen de suficientes fondos para realizar el pedido")
-    try:     
+    try:
+        if selected_food not in menu: 
+            raise ValueError("El alimento seleccionado no está en el menú")
+        if menu[selected_food] > money: 
+            raise ValueError("No se disponen de suficientes fondos para realizar el pedido")     
         total_price = menu[selected_food]
         print(f"Pedido realizado con éxito. Alimento seleccionado: {selected_food}, Total a pagar: {total_price}€")
-    except:
-        print("Error en el pedido: ValueError (asignado en las sentencias anteriores)")
+    except ValueError as err:
+        print(f"Error en el pedido: {err}")
 
 
 # Imprimir el menú
-for elem in menu:
-        print(f'{elem}: {menu[elem]}')
+def imprimir_menu():
+    print()
+    for elem in menu:
+            print(f'{elem}: {menu[elem]}')
 
 
 # Prueba 1
+imprimir_menu()
 selected_food = 'Pizza Margarita'
 money = 10
 place_order(selected_food, money) # Pedido realizado con éxito. Alimento seleccionado: Pizza Margarita, Total a pagar: 8.99€
 
 # Prueba 2
+imprimir_menu()
 selected_food = 'Pizza Margarita'
 money = 2
 place_order(selected_food, money) # ValueError: No se disponen de suficientes fondos para realizar el pedido
 
 # Prueba 3
+imprimir_menu()
 selected_food = 'Sandwich mixto'
 money = 10
 place_order(selected_food, money) # ValueError: El alimento seleccionado no está en el menú
